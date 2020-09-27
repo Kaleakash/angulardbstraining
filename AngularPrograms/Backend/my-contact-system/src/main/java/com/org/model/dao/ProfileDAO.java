c40bdc6d7536737577bb0f6efb28c557d44a938e
akash <akash300383@gmail.com>
2020-09-27 13:04:51 +0530

    Added Project

--- AngularPrograms/Backend/my-contact-system/src/main/java/com/org/model/dao/ProfileDAO.java
+++
@@ -1,8 +0,0 @@
-package com.org.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.org.model.beans.Profile;
-@EnableJpaRepositories
-public interface ProfileDAO extends JpaRepository<Profile, Integer>{}
